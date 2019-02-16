fc662b931323d58e09229cce2bc292e899146ac1
ANTOINEJCB <goopowo@gmail.com>
2019-02-16 18:03:43 +0100

    New - Init of the the WebApp.

---
+++ CatMash.Web/Models/CatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatMash.Web.Models
+{
+    public class CatViewModel
+    {
+        public string Identifier { get; set; }
+        public Uri ImageUrl { get; set; }
+        public int Votes { get; set; }
+    }
+}
