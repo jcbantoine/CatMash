6fe935f1a3809a090fa9293cffa5962e5b8032ee
ANTOINEJCB <goopowo@gmail.com>
2019-02-16 18:01:02 +0100

    New - Create EntityMashService with unit tests.

---
+++ EntityMash.Services.Tests/EntitiesMashServiceTests.cs
@@ -0,0 +1,99 @@
+using EntityMash.Models;
+using EntityMash.Services.Interop;
+using EntityMash.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+
+namespace EntityMash.Services.Tests
+{
+
+    [TestClass]
+    public class EntitiesMashServiceTests
+    {
+        [TestMethod]
+        public void EntitiesMashService_GetRanking()
+        {
+            //init
+            var entities = new[] { new Entity("") { Votes = 2 }, new Entity("") { Votes = 0 }, new Entity("") { Votes = 5 } };
+
+            var entitiesRepositoryMoq = new Mock<IEntitiesRepository>();
+            entitiesRepositoryMoq
+                .Setup(i => i.Entities)
+                .Returns(entities);
+
+            //system-under-test
+            var entitiesMashService = new EntityMashService(entitiesRepositoryMoq.Object);
+            var entitiesRanking = entitiesMashService.GetRanking();
+
+            //assert
+            Assert.AreEqual(5, entitiesRanking.ElementAt(0).Votes);
+            Assert.AreEqual(2, entitiesRanking.ElementAt(1).Votes);
+            Assert.AreEqual(0, entitiesRanking.ElementAt(2).Votes);
+
+        }
+
+        [TestMethod]
+        public void EntitiesMashService_AddVote()
+        {
+            //init
+            var entityToUpdate = new Entity("") { Votes = 2 };
+            var entities = new[] { entityToUpdate, new Entity("") { Votes = 0 }, new Entity("") { Votes = 5 } };
+
+            var entitiesRepositoryMoq = new Mock<IEntitiesRepository>();
+            entitiesRepositoryMoq
+                .Setup(i => i.Update(It.IsAny<Entity>()))
+                .Callback((Entity entity) => { entities[0] = entity; });
+
+            //system-under-test
+            var entitiesMashService = new EntityMashService(entitiesRepositoryMoq.Object);
+            entitiesMashService.AddVote(entityToUpdate);
+
+            //assert
+            entitiesRepositoryMoq.VerifyAll();
+            Assert.AreEqual(3, entities[0].Votes);
+        }
+
+        [TestMethod]
+        public void EntitiesMashService_GetVersus()
+        {
+            //init
+            var entity1 = new Entity("1") { Votes = 2 };
+            var entity2 = new Entity("2") { Votes = 2 };
+            var entity3 = new Entity("3") { Votes = 2 };
+            var entities = new[] { entity1, entity2, entity3 };
+
+            var entitiesRepositoryMoq = new Mock<IEntitiesRepository>();
+            entitiesRepositoryMoq
+                .Setup(i => i.Entities)
+                .Returns(entities);
+
+            //system-under-test
+            var entitiesMashService = new EntityMashService(entitiesRepositoryMoq.Object);
+            var versusTuple = entitiesMashService.GetVersus();
+
+            //assert
+            entitiesRepositoryMoq.VerifyAll();
+            Assert.AreNotEqual(versusTuple.Item1.Identifier, versusTuple.Item2.Identifier);
+        }
+
+        [ExpectedException(typeof(ApplicationException))]
+        [TestMethod]
+        public void EntitiesMashService_GetVersus_NotPossibleIfOneEntity()
+        {
+            //init
+            var entity1 = new Entity("1") { Votes = 2 };
+            var entities = new[] { entity1 };
+
+            var entitiesRepositoryMoq = new Mock<IEntitiesRepository>();
+            entitiesRepositoryMoq
+                .Setup(i => i.Entities)
+                .Returns(entities);
+
+            //system-under-test
+            var entitiesMashService = new EntityMashService(entitiesRepositoryMoq.Object);
+            var versusTuple = entitiesMashService.GetVersus();
+        }
+    }
+}
---
+++ EntityMash.Services/EntityMashService.cs
@@ -0,0 +1,70 @@
+﻿using EntityMash.Models;
+using EntityMash.Services.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntityMash.Services
+{
+    /// <summary>
+    /// This class is an implementation of <see cref="IEntitiesRepository"/>
+    /// </summary>
+    public class EntityMashService : IEntityMashService
+    {
+        private IEntitiesRepository entitiesRepository_ = null;
+
+        public EntityMashService(IEntitiesRepository entitiesRepository)
+        {
+            entitiesRepository_ = entitiesRepository;
+        }
+
+        /// <summary>
+        /// Implementation of <see cref="IEntityMashService.GetRanking"/>
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Entity> GetRanking()
+        {
+            return entitiesRepository_.Entities.OrderByDescending(c => c.Votes);
+        }
+
+        /// <summary>
+        /// Implementation of <see cref="IEntityMashService.GetVersus"/>
+        /// </summary>
+        /// <returns></returns>
+        public Tuple<Entity, Entity> GetVersus()
+        {
+            if (entitiesRepository_.Entities.Count() < 2)
+                throw new ApplicationException("Can't get a versus if there is only one entity.");
+            Entity entity1 = GetARandomEntity();
+            Entity entity2 = GetARandomEntity();
+            while (entity1.Identifier == entity2.Identifier)
+            {
+                entity1 = GetARandomEntity();
+                entity2 = GetARandomEntity();
+            }
+
+            return new Tuple<Entity, Entity>(entity1, entity2);
+        }
+
+        /// <summary>
+        /// Implementation of <see cref="IEntityMashService.AddVote(Entity)"/>
+        /// </summary>
+        /// <param name="entity"></param>
+        public void AddVote(Entity entity)
+        {
+            entity.Votes++;
+            entitiesRepository_.Update(entity);
+        }
+
+        /// <summary>
+        /// This private method retrieve an entity randomly.
+        /// </summary>
+        /// <returns></returns>
+        private Entity GetARandomEntity()
+        {
+            var random = new Random();
+            var index = random.Next(entitiesRepository_.Entities.Count() - 1);
+            return entitiesRepository_.Entities.ElementAt(index);
+        }
+    }
+}
