85537b4ca9e0ab6deec4dd14461d5a9dd101de64
ANTOINEJCB <goopowo@gmail.com>
2019-02-21 21:52:21 +0100

    New - Adding Utils to add new cats.

---
+++ EntityMash.Console.Utils/Program.cs
@@ -0,0 +1,35 @@
+﻿using EntityMash.Database;
+using EntityMash.Services;
+using System.Linq;
+
+namespace EntityMash.Console.Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length!=2 || (string.IsNullOrEmpty(args[0]) && string.IsNullOrEmpty(args[1])))
+            {
+                System.Console.WriteLine("An error occured when parsing your command.");
+                System.Console.WriteLine("Your command have to respect this format : ");
+                System.Console.WriteLine(">.exe [DBConnectionString] [PathToJson]");
+            }
+
+            var connectionString = args[0];
+            var jsonPath = args[1];
+            var dbContext = new EntityContext(connectionString);
+
+            var tempJsonRepo = new EntitiesRuntimeJsonRepository(jsonPath);
+
+            foreach(var entity in tempJsonRepo.Entities)
+            {
+                if(!dbContext.Entities.Any(e=>e.Identifier == entity.Identifier))
+                {
+                    dbContext.Entities.Add(entity.ToDBModel());
+                }
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
--- EntityMash.Database/EntityContext.cs
+++ EntityMash.Database/EntityContext.cs
@@ -1,15 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+
 using EntityMash.Database.Models;
 
 namespace EntityMash.Database
 {
     public class EntityContext : DbContext
     {
+        private readonly string connectionString_;
+
+        public EntityContext(string connectionString)
+        {
+            connectionString_ = connectionString;
+        }
+
         public EntityContext(DbContextOptions<EntityContext> options) : base(options)
         {
         }
 
         public DbSet<EntityDB> Entities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!string.IsNullOrEmpty(connectionString_))
+                optionsBuilder.UseSqlServer(connectionString_);
+        }
+
     }
 
 }
